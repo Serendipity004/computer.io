a7936ddd1bd147b563b3be07474ab4ebc98d428a
Serendipity004 <163754079+Serendipity004@users.noreply.github.com>
2024-04-01 10:59:58 +0800

    Update 10223901418_谢嘉茗.py

--- 10223901418_谢嘉茗.py
+++ 10223901418_谢嘉茗.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 #添加标题
-st.title('长三角PM2.5监测数据')
+st.title('社交媒体情感分析平台')
 
 #读取csv数据以dataframe显示
 url_data = "https://EcnuGISChaser.github.io/gis_development/data/csj_pm25.csv"
@@ -20,22 +20,22 @@ df1["lat"] = df1["纬度"]
 
 #创建表单
 with st.form("my_form"):
-    st.header("基于属性表达式查询记录")
+    st.header("基本信息收集表单")
 
 #两个selectbox
-#第一个“选择一个字段”
-    names1 = list(["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"])
-    name1 = st.selectbox('选择一个字段',names1)
+#第一个“性别”
+    gender = list(["男","女"])
+    gender1 = st.selectbox('选择一个字段',gender)
     
-#第二个“选择一个关系”
-    names2 = list(['>','<'])
-    name2 = st.selectbox('选择一个关系',names2)
+#第二个“年龄”
+    age = list(['>18','<18'])
+    age1 = st.selectbox('选择一个关系',age)
     
-#一个Text_input
-    txt = st.text_input('输入一个值',value="0",type="default")#type为缺省
+#一个Text_input昵称
+    txt = st.text_input('输入用户名',value="0",type="default")#type为缺省
     
 #一个表单提交按钮form_submit_button
-    submit_button = st.form_submit_button('提交')
+    submit_button = st.form_submit_button('数据上传')
 
     if submit_button:
         column_name = df.columns[df.columns.str.contains(name1)][0]
