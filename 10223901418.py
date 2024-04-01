c0f8d315a197953807325bb7bd747344faf21bab
Serendipity004 <163754079+Serendipity004@users.noreply.github.com>
2024-04-01 13:46:41 +0800

    Update 10223901418.py

--- 10223901418.py
+++ 10223901418.py
@@ -21,6 +21,22 @@ with st.form("my_form"):
 #一个表单提交按钮form_submit_button
     submit_button = st.form_submit_button('提交')
     
+
+with st.form("my_form2"):
+    st.header("基本信息收集表单")
+
+#第一个“性别”
+    gender = list(["男","女"])
+    gender1 = st.selectbox('性别',gender)
+    
+#第二个“年龄”
+    age1 = st.number_input('年龄',min_value=0,max_value=150,value=0,format="%d")
+    
+#一个Text_input昵称
+    txt = st.text_input('输入用户名',value="",type="default") #type为缺省
+    
+#一个表单提交按钮form_submit_button
+    submit_button = st.form_submit_button('提交')
     
     
 #读取csv数据以dataframe显示
