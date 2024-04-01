8788197b86a7511677ed12677e440ebef26605eb
Serendipity004 <163754079+Serendipity004@users.noreply.github.com>
2024-04-01 13:29:04 +0800

    Update 10223901418.py

--- 10223901418.py
+++ 10223901418.py
@@ -15,7 +15,7 @@ with st.form("my_form"):
     
 #第二个“年龄”
     age = list(['>18','<18'])
-    age1 = st.selectbox('年龄',age)
+    age1 = st.number_input('年龄',min_value=0,max_value=150,value=0,format="%d")
     
 #一个Text_input昵称
     txt = st.text_input('输入用户名',value="",type="default") #type为缺省
@@ -25,8 +25,15 @@ with st.form("my_form"):
 
 with st.form("my_form2"):
     st.header("数据收集表单")
-    txt = st.text_input('发表过文案',value="",type="default")
+    txt = st.text_area('发表过文案',value="",type="default")
     submit_button = st.form_submit_button('数据上传')
+    uploaded_file = st.file_uploader("上传文件", type=['txt', 'csv', 'xlsx'])
+    # 处理上传的文件
+    if uploaded_file is not None:
+        # 读取文件内容
+        file_contents = uploaded_file.read()
+        st.write(file_contents)
+    
     
 #读取csv数据以dataframe显示
 url_data = "https://EcnuGISChaser.github.io/gis_development/data/csj_pm25.csv"
